8fbae42108eae3fa7db9517f3e537b7a3a689c4e
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-07 12:24:32 +0100

    Added basic record class

--- Learning.Records/Program.cs
+++ Learning.Records/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Learning.Records.Classes;
+using Learning.Records.Records;
 using static System.Console;
 
 static void PrintCourse(Course course)
@@ -6,15 +7,32 @@ static void PrintCourse(Course course)
     WriteLine($"Course name: {course.Name}, created by: {course.Author}.");
 }
 
+static void PrintCourseRecord(CourseRecord course)
+{
+    WriteLine($"Course name: {course.Name}, created by: {course.Author}.");
+}
+
 var course = new Course()
 {
-    Name = "Basics of C#",
+    Name = "Basic C#",
     Author = "John Doe"
 };
 
 var anotherCourse = course;
 
-anotherCourse.Name = "Intricaties of C#"; // we're dealing with reference type!
+anotherCourse.Name = "Intricaties of C# classes"; // we're dealing with reference type!
 
 PrintCourse(course);
 PrintCourse(anotherCourse);
+
+
+var courseRecord = new CourseRecord("Advanced C#", "Alex Fox"); // positional record
+
+var anotherRecord = courseRecord;
+
+//anotherRecord.Name = "Intricaties of C# records"; // records are reference type, but :-) with a twist (immutable), init only property in this case
+
+anotherRecord = courseRecord with { Name = "Intricaties of C# records" }; // to change anything we must create a new object
+
+PrintCourseRecord(courseRecord);
+PrintCourseRecord(anotherRecord);
---
+++ Learning.Records/Records/CourseRecord.cs
@@ -0,0 +1,4 @@
+﻿namespace Learning.Records.Records
+{
+    public record CourseRecord(string Name, string Author);
+}

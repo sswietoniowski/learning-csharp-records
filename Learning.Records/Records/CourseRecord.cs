4ac05d45188f27be17c1295d0ca3cf0f7b05a5b1
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-07 13:42:05 +0100

    Tested new invention with C# 10 that is record struct

--- Learning.Records/Records/CourseRecord.cs
+++ Learning.Records/Records/CourseRecord.cs
@@ -1,6 +1,7 @@
 ﻿namespace Learning.Records.Records
 {
-    public record CourseRecord(string Name)
+    // keyword class is not required here (it is like that by default), I've added it to constract with record struct in declaration
+    public record class CourseRecord(string Name)
     {
         // constructor with parameters is created only for the positional parameters, 
         // if we decide to create custom record we must create it by ourself
@@ -18,5 +19,12 @@
 
         public string Author { get; init; } = default!;
         public string Title => $"{Name}, {Author}";
+    }
+
+    // starting with C# 10 we can have record structs (you know different management allocation amont other things ;-))
+    public record struct CourseRecordAsStruct
+    {
+        public string Name;
+        public string Author;
     }
 }
